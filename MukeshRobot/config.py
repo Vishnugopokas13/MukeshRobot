fa4037444f2915f607d5500408dc54dd51429828
Vishnugopokas13 <137295050+Vishnugopokas13@users.noreply.github.com>
2023-09-23 23:15:44 +0530

    Update config.py

--- MukeshRobot/config.py
+++ MukeshRobot/config.py
@@ -1,4 +1,3 @@
-
 class Config(object):
     LOGGER = True
     # REQUIRED
@@ -6,23 +5,23 @@ class Config(object):
 
     API_ID = "13305226" # integer value, dont use ""
     API_HASH = "8cde2475d6b0cb1162b89ebbac71a95d"
-    TOKEN = "5549516451:AAEBSzMz2PRUbcMAUggDQEzQS63RAomsn0Q"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 1258310642 # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    
-    SUPPORT_CHAT = "wmteams"  # Your own group for support, do not add the @
-    START_IMG = ""
+    TOKEN = "6592292827:AAHyz5q5MR9W_iRahcCjShG4UYRbCc-mRqw"    #is var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    OWNER_ID = 2145093972 # If you dont know, run the bot and do /id in your private chat with it, also an integer
+   
+    SUPPORT_CHAT = "wmteams" # Your own group for support, do not add the @
+    START_IMG = "https://graph.org/file/c3d867beff6d043e74f1f.jpg"
     EVENT_LOGS = ()  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
-    MONGO_DB_URI= ""
+    MONGO_DB_URI= "mongodb+srv://v:v@cluster0.zznxusa.mongodb.net/?retryWrites=true&w=majority"
     # RECOMMENDED
-    DATABASE_URL = "mongodb+srv://v:v@cluster0.zznxusa.mongodb.net/?retryWrites=true&w=majority"  # A sql database url from elephantsql.com
+    DATABASE_URL = ""  # A sql database url from elephantsql.com
     CASH_API_KEY = (
-        ""  # Get your API key from https://www.alphavantage.co/support/#api-key
+        "UX31XLHOBPL9KSGZ"  # Get your API key from https://www.alphavantage.co/support/#api-key
     )
-    TIME_API_KEY = ""
+    TIME_API_KEY = "OHBYT9I2Q0XH"
     # Get your API key from https://timezonedb.com/api
 
     # Optional fields
-    CHATBOT_API="" # get it from @FallenChat_Bot using /token
+    CHATBOT_API="-1001362823844" # get it from @FallenChat_Bot using /token
     BL_CHATS = []  # List of groups that you want blacklisted.
     DRAGONS = []  # User id of sudo users
     DEV_USERS = []  # User id of dev users
@@ -33,17 +32,3 @@ class Config(object):
     ALLOW_CHATS = True
     ALLOW_EXCL = True
     DEL_CMDS = True
-    INFOPIC = True
-    LOAD = []
-    NO_LOAD = []
-    STRICT_GBAN = True
-    TEMP_DOWNLOAD_DIRECTORY = "./"
-    WORKERS = 8
-    
-
-class Production(Config):
-    LOGGER = True
-
-
-class Development(Config):
-    LOGGER = True
